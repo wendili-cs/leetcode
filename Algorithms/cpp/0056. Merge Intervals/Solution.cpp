e158ed2e6c4ddd6eb7a1a3763ad541f02b015903
wendili-cs <wendili.academic@qq.com>
2022-10-13 19:57:09 -0500

    Update in 2022.10.13[x]

--- Algorithms/cpp/0056. Merge Intervals/Solution.cpp
+++ Algorithms/cpp/0056. Merge Intervals/Solution.cpp
@@ -1,33 +1,23 @@
 // https://leetcode-cn.com/problems/merge-intervals/
+// https://leetcode.com/problems/merge-intervals/
 
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        if(intervals.size()==0){
-            return {};
-        }
-        else if(intervals.size()==1){
-            return intervals;
-        }
-
-        vector<vector<int> > ans;
-        sort(intervals.begin(), intervals.end());
-
-        auto merge_interval = [](vector<int> &interval_a, vector<int> &interval_b) -> vector<int> {
-            return {min(interval_a[0], interval_b[0]), max(interval_a[1], interval_b[1])};
+        auto merge_intervals = [](vector<int> &a, vector<int> &b) -> vector<int> {
+            return {min(a[0], b[0]), max(a[1], b[1])};
         };
-
-        auto merging_interval = intervals[0];
-        for(int i = 1; i < intervals.size(); i++){
-            if(intervals[i][0] > merging_interval[1]){
-                ans.push_back(merging_interval);
-                merging_interval = intervals[i];
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> res;
+        for(auto &interval:intervals){
+            if(!res.empty() && res.back()[1] >= interval[0]){
+                vector<int> new_interval = merge_intervals(res.back(), interval);
+                res.back() = new_interval;
             }
             else{
-                merging_interval = merge_interval(intervals[i], merging_interval);
+                res.push_back(interval);
             }
         }
-        ans.push_back(merging_interval);
-        return ans;
+        return res;
     }
 };
